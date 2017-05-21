88bf252fa8afb9d5be83c07af2c6c22d30cd1da0
simon <z.simon94@gmail.com>
2017-05-21 01:20:19 -0400

    Add user agent feature

--- utils/web.py
+++ utils/web.py
@@ -3,9 +3,16 @@ import lxml.html as lh
 import urllib2
 # import pdb
 
+def __open_url_w_user_agent(url, user_agent='Mozilla/5.0'):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', user_agent)]
+    response = opener.open(url)
+    return response
+
 def doc_from_url(url):
     """Open url and prepare for parsing with lxml."""
-    return lh.parse(urllib2.urlopen(url))
+    resp = __open_url_w_user_agent(url)
+    return lh.parse(resp)
 
 def parse_with_filter(doc, elt, attr, val):
     """Return all elements from doc that match attr's value == val."""
