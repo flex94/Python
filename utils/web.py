6415cfd295359a81b6960f0915ab22647b969b12
flex <z.simon94@gmail.com>
2017-05-22 22:54:12 -0400

    Use only urllib, support POST

--- utils/web.py
+++ utils/web.py
@@ -1,24 +1,42 @@
 """Web utils."""
-import lxml.html as lh
-import urllib2
 # import pdb
+import lxml.html as lh
+import urllib
+import urllib2
 
-def __open_url_w_user_agent(url, user_agent='Mozilla/5.0'):
+def open_url(url, data={}, user_agent='Mozilla/5.0'):
+    """Get a webpage content from its url.
+
+    Returns a response ready to be parsed with lxml.
+    data: POST data (optional)
+    user_agent: defaults to Mozilla (optional)
+    """
     opener = urllib2.build_opener()
     opener.addheaders = [('User-Agent', user_agent)]
-    response = opener.open(url)
+
+    if len(data):
+        response = opener.open(url, urllib.urlencode(data))
+    else:
+        response = opener.open(url)
+
     return response
 
-def doc_from_url(url):
-    """Open url and prepare for parsing with lxml."""
-    resp = __open_url_w_user_agent(url)
-    return lh.parse(resp)
+def parse_with_filter(resp, elt, attr, val):
+    """Return all elements from the page that match attr's value == val.
 
-def parse_with_filter(doc, elt, attr, val):
-    """Return all elements from doc that match attr's value == val."""
+    resp: urllib2 opener - use result from open_url
+    elt: string - e.g. div, a
+    attr: string - e.g. class, id
+    val: string
+    """
+    doc = lh.parse(resp)
+
     return filter(lambda e: e.get(attr) == val, doc.iter(elt))
 
+
 # Examples
-# url = 'http://9docu.com'
-# doc = doc_from_url(url)
-# resu = parse_with_filter(doc, 'div', 'class', 'data')
+# url = 'http://cpabien.xyz/search.php'
+# data = {'t': 'muraille'}
+# resp = open_url(url, data)
+# resu = parse_with_filter(resp, 'a', 'class', 'titre')
+# pdb.set_trace()
