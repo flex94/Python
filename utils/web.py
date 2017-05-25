a2458073db5afbee791b4ca9289c48b29f9dd14a
flex <z.simon94@gmail.com>
2017-05-24 21:27:31 -0400

    Support parsing with a filtr fn, and add dl_file

--- utils/web.py
+++ utils/web.py
@@ -1,13 +1,17 @@
 """Web utils."""
-# import pdb
 import lxml.html as lh
 import urllib
 import urllib2
+import requests
+from shutil import copyfileobj
 
-def open_url(url, data={}, user_agent='Mozilla/5.0'):
+USER_AGENT_MOZILLA = 'Mozilla/5.0'
+
+def open_url(url, data={}, user_agent=USER_AGENT_MOZILLA):
     """Get a webpage content from its url.
 
     Returns a response ready to be parsed with lxml.
+
     data: POST data (optional)
     user_agent: defaults to Mozilla (optional)
     """
@@ -21,22 +25,47 @@ def open_url(url, data={}, user_agent='Mozilla/5.0'):
 
     return response
 
-def parse_with_filter(resp, elt, attr, val):
+def parse_with_filter(resp, elt, attr, f):
     """Return all elements from the page that match attr's value == val.
 
     resp: urllib2 opener - use result from open_url
     elt: string - e.g. div, a
     attr: string - e.g. class, id
-    val: string
+    f: string or lambda
     """
     doc = lh.parse(resp)
 
-    return filter(lambda e: e.get(attr) == val, doc.iter(elt))
+    if callable(f):
+        def filtr(e):
+            return f(e.get(attr))
+    else:
+        def filtr(e):
+            return e.get(attr) == f
+
+    return filter(filtr, doc.iter(elt))
+
+def dl_file(url, name, user_agent=USER_AGENT_MOZILLA):
+    """Download file from url and write to current folder."""
+    req = requests.get(url, stream=True, headers={'User-Agent': user_agent})
+    if req.status_code == 200:
+        with open(name, 'wb') as f:
+            req.raw.decode_content = True
+            copyfileobj(req.raw, f)
+        return True
+    return False
 
 
-# Examples
-# url = 'http://cpabien.xyz/search.php'
-# data = {'t': 'muraille'}
-# resp = open_url(url, data)
-# resu = parse_with_filter(resp, 'a', 'class', 'titre')
-# pdb.set_trace()
+""" Examples
+
+url = 'http://cpabien.xyz/search.php'
+data = {'t': 'muraille'}
+resp = open_url(url, data)
+
+def filtr(val):
+    return (type(val) is str and 'ligne' in val)
+resu = parse_with_filter(resp, 'div', 'class', filtr)
+
+Then can do resu[0][0].get('href')...
+
+"""
+
