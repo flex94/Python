24f1edbeb4a1963e70b5e16339f6291ad8cac075
flex94 <z.simon94@gmail.com>
2017-06-04 16:18:46 -0400

    Add clean_str unicode support, use it for dl_title

--- utils/strings.py
+++ utils/strings.py
@@ -4,7 +4,14 @@
 import unicodedata
 
 def clean_str(s):
-    """Remove accents and encode in utf-8."""
-    s = unicode(s, 'utf-8')
+    """Remove all accents from an input string or unicode.
+
+	Use after receiving an input from user or before printing to console.
+	s: utf-8 encoded string or unicode, with accents
+	return: ascii encoded string without accents
+	"""
+    if not isinstance(s, unicode):
+	    s = unicode(s, 'utf-8')
+
     s = unicodedata.normalize('NFD', s).encode('ascii', 'ignore')
     return s
